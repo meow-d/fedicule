39b54af3a4b47123cbbe27108c0d22754b64bec6
meow-d <meow_d@tuta.io>
2025-01-09 15:10:00 +0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  base: "/fedicule/",
   server: {
     host: "127.0.0.1",
     port: 3000,
