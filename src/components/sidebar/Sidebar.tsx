88395b46b3c3c533649cb13943789d4d0fe04b48
meow-d <meow_d@tuta.io>
2025-01-31 11:18:41 +0800

    feat: collapsable sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,26 +2,32 @@ import logo from "../../assets/logo.svg";
 import styles from "./Sidebar.module.css";
 
 import DataSection from "./DataSection";
-import FetchSection from "./FetchSection";
 import SettingsSection from "./SettingsSection";
+import { createSignal, Show } from "solid-js";
 
 export default function Sidebar() {
-  let collapsed = false;
+  let [collapsed, setCollapsed] = createSignal(false);
+  const toggleCollapsed = () => {
+    setCollapsed((prev) => !prev);
+  };
 
   return (
-    <aside>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <h1>Fedicule</h1>
-      </header>
+    <aside class={collapsed() ? styles.collasped : ""}>
+      <button id={styles.collapseButton} onClick={toggleCollapsed}>
+        &gt;&gt;
+      </button>
 
-      <div class={styles.content}>
-        <DataSection />
-        {/* TODO: delete */}
-        {/* <FetchSection /> */}
-        <SettingsSection />
-      </div>
-      <button id={styles.collapseButton}>aaa</button>
+      <Show when={!collapsed()}>
+        <header class={styles.header}>
+          <img src={logo} class={styles.logo} alt="logo" />
+          <h1>Fedicule</h1>
+        </header>
+
+        <div class={styles.content}>
+          <DataSection />
+          <SettingsSection />
+        </div>
+      </Show>
     </aside>
   );
 }
