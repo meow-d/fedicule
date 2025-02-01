94da72e4cd14ff7fcf5943e7540bad32d49463cd
meow-d <meow_d@tuta.io>
2025-02-01 11:30:35 +0800

    feat: add, fix, and refactor "validation"

--- src/components/sidebar/DataSection.tsx
+++ src/components/sidebar/DataSection.tsx
@@ -1,15 +1,11 @@
-import { createSignal, onMount, Show } from "solid-js";
+import { createEffect, createMemo, createSignal, onMount, Show } from "solid-js";
+import { createStore } from "solid-js/store";
 
 import { update } from "../graph/Graph";
 
 import { MastoClient } from "../../lib/masto/client";
 
-import {
-  createApp,
-  getToken,
-  redirectToInstance,
-  revokeToken,
-} from "../../lib/masto/auth";
+import { createApp, getToken, redirectToInstance, revokeToken } from "../../lib/masto/auth";
 
 import Section from "../ui/Section";
 import Checkbox from "../ui/Checkbox";
@@ -17,23 +13,78 @@ import Message from "../ui/Message";
 import Button from "../ui/Button";
 
 import { auth, setAuth } from "../../stores/authStore";
-import { loading, setLoading } from "../../stores/loading";
 import { data, setData } from "../../stores/data";
 import { Client } from "../../lib/Client";
 import { BskyClient } from "../../lib/bsky/client";
 
 export default function DataSection() {
-  const [message, setMessage] = createSignal("");
-  const [isError, setIsError] = createSignal(false);
-  const [fetchEnabled, setFetchEnabled] = createSignal(true);
-  const [fetchVisible, setFetchVisible] = createSignal(true);
-  const [loginVisible, setLoginVisible] = createSignal(true);
-  let homeCheckbox, followsCheckbox, fetchAmount, apiSelect, handleInput;
+  // status
+  const [status, setStatus] = createStore({ message: "", error: false, loading: false });
 
+  // fetch
+  const startFetch = async () => {
+    const api = inputs.api;
+    const handle = inputs.handle;
+    const follows = inputs.follows;
+    const home = inputs.home;
+    const postsNo = inputs.postsNo;
+
+    if (!handle) {
+      throw new Error("Fields are not filled in");
+    }
+    if (!follows && !home) {
+      setStatus({ message: "Please select at least one source", error: true, loading: false });
+      return;
+    }
+
+    setData({ processedData: undefined });
+
+    let client;
+    if (api === "mastoapi") {
+      client = new MastoClient();
+    } else if (api === "bsky") {
+      client = await BskyClient.create(handle);
+    } else {
+      throw new Error("Not implemented");
+    }
+
+    setStatus("loading", true);
+    client.onProgress((update) => setStatus("message", update));
+
+    try {
+      if (follows) await fetchFollows(client as Client);
+      if (home) await fetchPosts(client as Client, postsNo);
+      update();
+    } catch (error: any) {
+      console.error(error);
+      setStatus({
+        message: error.message,
+        error: true,
+        loading: false,
+      });
+    }
+  };
+
+  const fetchFollows = async (client: Client) => {
+    const processedData = await client.fetchFollows();
+    setData({ processedData });
+  };
+
+  const fetchPosts = async (client: Client, postsNo: number) => {
+    if (!postsNo) throw new Error("Please enter a number");
+
+    const processedData = await client.fetchFeed(postsNo);
+    if (data.processedData) {
+      processedData.interaction = data.processedData.interaction.concat(processedData.interaction);
+    }
+    setData({ processedData });
+  };
+
+  // login
   const mastoLogin = async () => {
     try {
-      const handle = (handleInput as unknown as HTMLInputElement).value;
-      const api = (apiSelect as unknown as HTMLSelectElement).value;
+      const handle = inputs.handle;
+      const api = inputs.api;
 
       if (!handle || !api) {
         throw new Error("Fields are not filled in");
@@ -50,92 +101,13 @@ export default function DataSection() {
         throw new Error("Invalid handle");
       }
 
-      setLoading(true);
-      setIsError(false);
-      setMessage("Creating app...");
       setAuth(await createApp(instance, auth));
 
       setAuth({ handle });
-      setMessage("Redirecting to your instance...");
       redirectToInstance(auth);
     } catch (error: any) {
       console.error(error);
-      setIsError(true);
-      setMessage(error.message);
-      setLoading(false);
     }
-  };
-
-  const startFetch = async () => {
-    if (!homeCheckbox || !followsCheckbox || !fetchAmount || !apiSelect) return;
-
-    const isFollowsChecked = (followsCheckbox as HTMLInputElement).checked;
-    const isHomeChecked = (homeCheckbox as HTMLInputElement).checked;
-    const api = (apiSelect as unknown as HTMLSelectElement).value;
-    const handle = (handleInput as unknown as HTMLInputElement).value;
-    if (!handle) {
-      throw new Error("Fields are not filled in");
-    }
-    if (!isFollowsChecked && !isHomeChecked) {
-      setMessage("Please select at least one source");
-      setIsError(true);
-      return;
-    }
-
-    setData({ processedData: undefined });
-
-    let client;
-    if (api === "mastoapi") {
-      client = new MastoClient();
-    } else if (api === "bsky") {
-      client = await BskyClient.create(handle);
-    } else {
-      throw new Error("Not implemented");
-    }
-
-    client.onProgress((update) => setMessage(update));
-
-    try {
-      if (isFollowsChecked) await fetchFollows(client as Client);
-      if (isHomeChecked) await fetchPosts(client as Client);
-      update();
-    } catch (error: any) {
-      console.error(error);
-      setStatus(error.message, true, false);
-    }
-  };
-
-  const fetchFollows = async (client: Client) => {
-    const processedData = await client.fetchFollows();
-    setData({ processedData });
-  };
-
-  const fetchPosts = async (client: Client) => {
-    if (!fetchAmount) return;
-
-    const postCount = (fetchAmount as HTMLInputElement).value;
-    const postCountInt = parseInt(postCount);
-    if (!postCount || isNaN(postCountInt)) {
-      throw new Error("Please enter a number");
-    }
-
-    const processedData = await client.fetchFeed(postCountInt);
-    if (data.processedData) {
-      processedData.interaction = data.processedData.interaction.concat(
-        processedData.interaction,
-      );
-    }
-    setData({ processedData });
-  };
-
-  const setStatus = (
-    newMessage: string,
-    error?: boolean,
-    loading?: boolean,
-  ) => {
-    setIsError(error || false);
-    setMessage(newMessage);
-    setLoading(loading || true);
   };
 
   onMount(async () => {
@@ -144,61 +116,75 @@ export default function DataSection() {
     if (!code) return;
 
     try {
-      setLoading(false);
-      setIsError(false);
-      setMessage("Getting token from code...");
+      setStatus({
+        message: "Getting token from code...",
+        error: false,
+        loading: true,
+      });
       window.history.replaceState(null, "hello safari user", "/");
       setAuth(await getToken(code, auth));
-      setMessage("");
+      setStatus({
+        message: "Logged in..",
+        error: false,
+        loading: false,
+      });
     } catch (error: any) {
       console.error(error);
-      setIsError(true);
-      setMessage(error.message);
-      setLoading(false);
+      setStatus({
+        message: error.message,
+        error: true,
+        loading: false,
+      });
     }
   });
 
   const logout = async () => {
     try {
-      setIsError(false);
-      setLoading(true);
-      setMessage("Logging out...");
-
+      setStatus({ message: "Logging out...", error: false, loading: true });
+      // TODO: should be tied to client
       setAuth(await revokeToken(auth));
-
-      setLoading(false);
-      setMessage("");
+      setStatus({ message: "Logged out", error: false, loading: false });
     } catch (error: any) {
       console.error(error);
-      setIsError(true);
-      setMessage(error.message);
-      setLoading(false);
+      setStatus({ message: error.message, error: true, loading: false });
     }
   };
 
-  const updateButtonStates = () => {
-    const follow = followsCheckbox as unknown as HTMLInputElement;
-    const home = homeCheckbox as unknown as HTMLInputElement;
-    const api = apiSelect as unknown as HTMLSelectElement;
+  // UI
+  interface Inputs {
+    api: "mastoapi" | "bsky";
+    handle: string;
+    follows: boolean;
+    home: boolean;
+    postsNo: number;
+  }
+  const [inputs, setInputs] = createStore<Inputs>({
+    follows: true,
+    home: false,
+    api: "mastoapi",
+    handle: auth.handle ?? "",
+    postsNo: 100,
+  });
 
-    setFetchEnabled(follow.checked || home.checked);
-    setFetchVisible(api.value !== "mastoapi" || auth.loggedIn);
-    setLoginVisible(api.value != "bsky");
-  };
-  onMount(updateButtonStates);
+  const postsNoVisible = createMemo(() => inputs.home);
+  const loginVisible = createMemo(() => inputs.api === "mastoapi" && !auth.loggedIn);
+  const fetchEnabled = createMemo(() => inputs.follows || inputs.home);
+  const fetchVisible = createMemo(() => inputs.api !== "mastoapi" || !!auth.loggedIn);
 
   return (
     <Section title="Data" open={!data.processedData}>
+      {/* inputs */}
       <div>
         <label for="api">API</label>
         <select
           name="api"
           id="api"
-          disabled={loading() || auth.loggedIn}
-          ref={apiSelect}
-          onChange={updateButtonStates}
+          onChange={(e) => setInputs("api", e.target.value as "mastoapi" | "bsky")}
+          disabled={status.loading || auth.loggedIn}
         >
-          <option value="mastoapi">Mastodon</option>
+          <option value="mastoapi" selected>
+            Mastodon
+          </option>
           <option value="bsky">Bluesky</option>
           {/* TODO */}
           {/* <option value="misskey">Misskey</option> */}
@@ -211,9 +197,9 @@ export default function DataSection() {
           type="text"
           name="handle"
           id="handle"
-          value={auth.handle ? auth.handle : ""}
-          ref={handleInput}
-          disabled={loading() || auth.loggedIn}
+          value={inputs.handle}
+          onChange={(e) => setInputs("handle", e.target.value)}
+          disabled={status.loading || auth.loggedIn}
         />
       </div>
 
@@ -221,52 +207,51 @@ export default function DataSection() {
         <label>Sources</label>
         <Checkbox
           name="follows"
-          ref={followsCheckbox}
-          onChange={updateButtonStates}
-          checked={true}
+          onChange={(e) => setInputs("follows", e.target.checked)}
+          checked={inputs.follows}
         ></Checkbox>
-        <Checkbox
-          name="home"
-          displayName="home feed"
-          ref={homeCheckbox}
-          onChange={updateButtonStates}
-        ></Checkbox>
+        <Checkbox name="home" displayName="home feed" onChange={(e) => setInputs("home", e.target.checked)}></Checkbox>
       </div>
 
-      <div>
-        <label for="fetch-amount">Home feed posts no.</label>
-        <input
-          type="number"
-          name="fetch-amount"
-          id="fetch-amount"
-          value="100"
-          ref={fetchAmount}
-          onChange={updateButtonStates}
-          disabled={loading()}
-        />
-      </div>
+      <Show when={postsNoVisible}>
+        <div>
+          <label for="fetch-amount">Home feed posts no.</label>
+          <input
+            type="number"
+            name="fetch-amount"
+            id="fetch-amount"
+            value={inputs.postsNo}
+            onChange={(e) => setInputs("postsNo", parseInt(e.target.value))}
+            disabled={status.loading}
+          />
+        </div>
+      </Show>
 
+      {/* buttons */}
       <div>
-        <Show when={loginVisible() && !auth.loggedIn}>
-          <Button disabled={loading()} onclick={mastoLogin}>
-            login
-          </Button>
-        </Show>
-
-        <Show when={auth.loggedIn}>
-          <Button disabled={loading()} onclick={logout}>
+        <Show
+          when={auth.loggedIn}
+          fallback={
+            <Show when={loginVisible()}>
+              <Button disabled={status.loading} onclick={mastoLogin}>
+                login
+              </Button>
+            </Show>
+          }
+        >
+          <Button disabled={status.loading} onclick={logout}>
             logout
           </Button>
         </Show>
 
         <Show when={fetchVisible()}>
-          <Button disabled={!fetchEnabled() || loading()} onClick={startFetch}>
+          <Button disabled={!fetchEnabled() || status.loading} onClick={startFetch}>
             {data.mastoFeedRaw ? "refetch" : "fetch"}
           </Button>
         </Show>
 
-        <Show when={message()}>
-          <Message message={message()} isError={isError()} />
+        <Show when={status.message}>
+          <Message message={status.message} isError={status.error} />
         </Show>
       </div>
     </Section>
--- src/stores/loading.tsx
+++
@@ -1,7 +0,0 @@
-// >./stores
-// >looks inside
-// >signal
-
-import { createSignal } from "solid-js";
-
-export const [loading, setLoading] = createSignal(false);
