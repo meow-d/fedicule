9a4af58f05d4eb3528bbb567da2e4bd8d9d8ea49
meow-d <meow_d@tuta.io>
2025-02-01 18:12:39 +0800

    chore: remove unused code

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -1,12 +1,6 @@
 import { ProcessedData } from "../stores/data";
 
 export abstract class Client {
-  // factory method
-  // idk what else to do..
-  static async create(handle: string): Promise<Client> {
-    throw new Error("Not implemented");
-  }
-
   private progressListener: ((update: string) => void) | null = null;
   onProgress(listener: (update: string) => void) {
     this.progressListener = listener;
